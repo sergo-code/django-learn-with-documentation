a8e4371687a0bb0b959b649eab2c4bc8a4c50525
sergo-code <s.ivanoff-71@yandex.ru>
2022-08-07 15:38:16 +0600

    Updated views, added forms

--- relationships/views.py
+++ relationships/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse, HttpResponseNotFound
 import datetime
 
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods, require_GET, require_POST, require_safe
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
+from .forms import UploadFileForm
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # file is saved
+            form.save()
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+
+@require_http_methods(["GET", "POST"])
 def relationship_m2m(request, year):
     if year == 1999:
         return HttpResponseNotFound('<h1>Page not found</h1>')
